a9e5b7f15a4be79d5945a9b4e7f96554b67350ac
adams <a4szczepaniak@gmail.com>
2020-04-28 15:48:30 +0100

    Init
    Signed-off-by: adams <a4szczepaniak@gmail.com>

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PracticePage from "./PracticePage";
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <PracticePage/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PracticePage.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+
+class PracticePage extends React.Component {
+
+    render(){
+        return (<h2>GP Practices</h2>);
+    }
+}
+
+export default PracticePage;
